4bbc55e00c1f3e38fc6dc7d55def57ed26d095b2
sarah <hamdisarah61@gmail.com>
2024-12-28 23:40:51 +0100

    Ajout de mon code

--- Nurse.java
+++
@@ -1,7 +0,0 @@
-public class Nurse extends Person {
-
-    public Nurse( int id, String lastName, String name, String phoneNumber) {
-        super(id, name, lastName, phoneNumber);  // Call the constructor of the Person class
-    }
-
-}
