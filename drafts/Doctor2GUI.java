a2e6a5cd79c96973204bb1dbb24384ffb471e0e5
Sarah <155466995+hamdisarah@users.noreply.github.com>
2025-02-06 21:08:26 +0100

    Update Doctor2GUI.java

--- drafts/Doctor2GUI.java
+++ drafts/Doctor2GUI.java
@@ -1,20 +1,9 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-// Specific graphic interface for doctor 1
+// NOUVELLE VERSION POUR L'INTERFACE GRAPHIQUE DU MEDECIN 1 
+// QUAND CE CODE S'EXECUTE, ET ON CLIQUE SUR DOCTOR 2, SON CALENDRIER S'AFFICHE DANS UNE AUTRE FENETRE 
+// LE JSplitPane POUR CE MEDECIN JE LE FERAI UNE FOIS LA PARTIE DU MEDECIN 1 DONNE DE BON RESULTAT 
+
 public class Doctor2GUI extends CalendarGUI {
-    public Doctor2GUI(Doctor2Calendar calendar) {
-        super(calendar);
-        createGUI(); // Call to the CalendarGUI constructor to initialize the calendar
-    }
-  @Override
-  public void createGUI() {
-    JFrame frame = new JFrame("Calendar Doctor 2: Gynecologist");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(400, 400);
-    frame.setLayout(new BorderLayout());
-    // Appel de la méthode `createGUI()` de la classe parent
-    super.createGUI();
-    frame.setVisible(true); // Afficher la fenêtre
-}
+  public Doctor2GUI(CalendarINFO calendarInfo) {
+      super(calendarInfo, "Doctor 2 Calendar"); // Passe un autre titre au parent
+  }
 }
